5e928eb8b1ace40f680175f70ea8622f19359d9c
AndreyZhitenev <zhitenev.andr@gmail.com>
2022-07-18 17:37:21 +0300

    branding component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
-	<router-view />
+	<div>
+		<div class="app flex flex-column">
+			<Navigation />
+			<div class="app-content flex flex-column"><router-view /></div>
+		</div>
+	</div>
 </template>
+
+<script>
+import Navigation from "./components/Navigation";
+export default {
+	components: {
+		Navigation,
+	},
+	methods: {},
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
@@ -10,7 +25,19 @@
 	padding: 0;
 	box-sizing: border-box;
 	font-family: "Poppins", sans-serif;
+}
+
+.app {
 	background-color: #141625;
+	min-height: 100vh;
+	@media (min-width: 900px) {
+		flex-direction: row !important;
+	}
+	.app-content {
+		padding: 0 20px;
+		flex: 1;
+		position: relative;
+	}
 }
 
 button,

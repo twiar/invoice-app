981f0df767aa03811bc0844d829d7bb67f44dbd5
AndreyZhitenev <zhitenev.andr@gmail.com>
2022-07-19 20:47:34 +0300

    pop-up modal component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 		<div v-if="!mobile" class="app flex flex-column">
 			<Navigation />
 			<div class="app-content flex flex-column">
+				<Modal v-if="modalActive" />
 				<transition name="invoice">
 					<InvoiceModal v-if="invoiceModal" />
 				</transition>
@@ -20,6 +21,7 @@
 import { mapState } from "vuex";
 import Navigation from "./components/Navigation";
 import InvoiceModal from "./components/InvoiceModal";
+import Modal from "./components/Modal";
 export default {
 	data() {
 		return {
@@ -29,6 +31,7 @@ export default {
 	components: {
 		Navigation,
 		InvoiceModal,
+		Modal,
 	},
 	created() {
 		this.checkScreen();
@@ -45,7 +48,7 @@ export default {
 		},
 	},
 	computed: {
-		...mapState(["invoiceModal"]),
+		...mapState(["invoiceModal", "modalActive"]),
 	},
 };
 </script>

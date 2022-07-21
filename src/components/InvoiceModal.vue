e4914a81bdffd8e6ebad2ab74dcf0a9438abb84f
AndreyZhitenev <zhitenev.andr@gmail.com>
2022-07-21 20:07:07 +0300

    edit invoice

--- src/components/InvoiceModal.vue
+++ src/components/InvoiceModal.vue
@@ -2,7 +2,8 @@
 	<div @click="checkClick" ref="invoiceWrap" class="invoice-wrap flex flex-column">
 		<form @submit.prevent="submitForm" class="invoice-content">
 			<Loading v-show="loading" />
-			<h1>New Invoice</h1>
+			<h1 v-if="!editInvoice">New Invoice</h1>
+			<h1 v-else>Edit Invoice</h1>
 
 			<!-- Bill From -->
 			<div class="bill-from flex flex-column">
@@ -112,8 +113,13 @@
 					<button type="button" @click="closeInvoice" class="red">Cancel</button>
 				</div>
 				<div class="right flex">
-					<button type="submit" @click="saveDraft" class="dark-purple">Save Draft</button>
-					<button type="submit" @click="publishInvoice" class="purple">Create Invoice</button>
+					<button v-if="!editInvoice" type="submit" @click="saveDraft" class="dark-purple">
+						Save Draft
+					</button>
+					<button v-if="!editInvoice" type="submit" @click="publishInvoice" class="purple">
+						Create Invoice
+					</button>
+					<button v-if="editInvoice" type="submit" class="purple">Update Invoice</button>
 				</div>
 			</div>
 		</form>
@@ -123,13 +129,14 @@
 <script>
 import db from "../firebase/firebaseInit";
 import Loading from "../components/Loading";
-import { mapMutations } from "vuex";
+import { mapActions, mapMutations, mapState } from "vuex";
 import { uid } from "uid";
 export default {
 	name: "invoiceModal",
 	data() {
 		return {
 			dateOptions: { year: "numeric", month: "short", day: "numeric" },
+			docId: null,
 			loading: null,
 			billerStreetAddress: null,
 			billerCity: null,
@@ -158,11 +165,43 @@ export default {
 	},
 	created() {
 		// get current date for invoice date field
-		this.invoiceDateUnix = Date.now();
-		this.invoiceDate = new Date(this.invoiceDateUnix).toLocaleDateString("en-us", this.dateOptions);
+		if (!this.editInvoice) {
+			this.invoiceDateUnix = Date.now();
+			this.invoiceDate = new Date(this.invoiceDateUnix).toLocaleDateString(
+				"en-us",
+				this.dateOptions,
+			);
+		}
+
+		if (this.editInvoice) {
+			const currentInvoice = this.currentInvoiceArray[0];
+			this.docId = currentInvoice.docId;
+			this.billerStreetAddress = currentInvoice.billerStreetAddress;
+			this.billerCity = currentInvoice.billerCity;
+			this.billerZipCode = currentInvoice.billerZipCode;
+			this.billerCountry = currentInvoice.billerCountry;
+			this.clientName = currentInvoice.clientName;
+			this.clientEmail = currentInvoice.clientEmail;
+			this.clientStreetAddress = currentInvoice.clientStreetAddress;
+			this.clientCity = currentInvoice.clientCity;
+			this.clientZipCode = currentInvoice.clientZipCode;
+			this.clientCountry = currentInvoice.clientCountry;
+			this.invoiceDateUnix = currentInvoice.invoiceDateUnix;
+			this.invoiceDate = currentInvoice.invoiceDate;
+			this.paymentTerms = currentInvoice.paymentTerms;
+			this.paymentDueDateUnix = currentInvoice.paymentDueDateUnix;
+			this.paymentDueDate = currentInvoice.paymentDueDate;
+			this.productDescription = currentInvoice.productDescription;
+			this.invoicePending = currentInvoice.invoicePending;
+			this.invoiceDraft = currentInvoice.invoiceDraft;
+			this.invoiceItemList = currentInvoice.invoiceItemList;
+			this.invoiceTotal = currentInvoice.invoiceTotal;
+		}
 	},
 	methods: {
-		...mapMutations(["TOGGLE_INVOICE", "TOGGLE_MODAL"]),
+		...mapMutations(["TOGGLE_INVOICE", "TOGGLE_MODAL", "TOGGLE_EDIT_INVOICE"]),
+
+		...mapActions(["UPDATE_INVOICE"]),
 
 		checkClick(e) {
 			if (e.target === this.$refs.invoiceWrap) {
@@ -172,6 +211,9 @@ export default {
 
 		closeInvoice() {
 			this.TOGGLE_INVOICE();
+			if (this.editInvoice) {
+				this.TOGGLE_EDIT_INVOICE();
+			}
 		},
 		addNewInvoiceItem() {
 			this.invoiceItemList.push({
@@ -242,9 +284,57 @@ export default {
 			this.TOGGLE_INVOICE();
 		},
 
+		async updateInvoice() {
+			if (this.invoiceItemList.length <= 0) {
+				alert("Please ensure you filled out work items!");
+				return;
+			}
+
+			this.loading = true;
+
+			this.calInvoiceTotal();
+
+			const dataBase = db.collection("invoices").doc(this.docId);
+
+			await dataBase.update({
+				billerStreetAddress: this.billerStreetAddress,
+				billerCity: this.billerCity,
+				billerZipCode: this.billerZipCode,
+				billerCountry: this.billerCountry,
+				clientName: this.clientName,
+				clientEmail: this.clientEmail,
+				clientStreetAddress: this.clientStreetAddress,
+				clientCity: this.clientCity,
+				clientZipCode: this.clientZipCode,
+				clientCountry: this.clientCountry,
+				paymentTerms: this.paymentTerms,
+				paymentDueDate: this.paymentDueDate,
+				paymentDueDateUnix: this.paymentDueDateUnix,
+				productDescription: this.productDescription,
+				invoiceItemList: this.invoiceItemList,
+				invoiceTotal: this.invoiceTotal,
+			});
+
+			this.loading = false;
+
+			const data = {
+				docId: this.docId,
+				routeId: this.$route.params.invoiceId,
+			};
+
+			this.UPDATE_INVOICE(data);
+		},
+
 		submitForm() {
+			if (this.editInvoice) {
+				this.updateInvoice();
+				return;
+			}
 			this.uploadInvoice();
 		},
+	},
+	computed: {
+		...mapState(["editInvoice", "currentInvoiceArray"]),
 	},
 	watch: {
 		paymentTerms() {
--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -21,7 +21,7 @@
 				</div>
 			</div>
 			<div class="right flex">
-				<button @click="toggleEditInvoice(currentInvoice.docId)" class="dark-purple">Edit</button>
+				<button @click="toggleEditInvoice" class="dark-purple">Edit</button>
 				<button @click="toggleEditInvoice(currentInvoice.docId)" class="red">Delete</button>
 				<button
 					v-if="currentInvoice.invoicePending"
@@ -65,6 +65,7 @@
 				</div>
 				<div class="bill flex flex-column">
 					<h4>Bill To</h4>
+					<p>{{ currentInvoice.clientName }}</p>
 					<p>{{ currentInvoice.clientStreetAddress }}</p>
 					<p>{{ currentInvoice.clientCity }}</p>
 					<p>{{ currentInvoice.clientZipCode }}</p>
@@ -116,15 +117,27 @@ export default {
 		this.getCurrentInvoice();
 	},
 	methods: {
-		...mapMutations(["SET_CURRENT_INVOICE"]),
+		...mapMutations(["SET_CURRENT_INVOICE", "TOGGLE_EDIT_INVOICE", "TOGGLE_INVOICE"]),
 
 		getCurrentInvoice() {
 			this.SET_CURRENT_INVOICE(this.$route.params.invoiceId);
 			this.currentInvoice = this.currentInvoiceArray[0];
 		},
+
+		toggleEditInvoice() {
+			this.TOGGLE_EDIT_INVOICE();
+			this.TOGGLE_INVOICE();
+		},
 	},
 	computed: {
-		...mapState(["currentInvoiceArray"]),
+		...mapState(["currentInvoiceArray", "editInvoice"]),
+	},
+	watch: {
+		editInvoice() {
+			if (!this.editInvoice) {
+				this.currentInvoice = this.currentInvoiceArray[0];
+			}
+		},
 	},
 };
 </script>
